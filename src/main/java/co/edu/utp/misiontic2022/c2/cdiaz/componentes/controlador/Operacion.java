09103cfc244bc8ace1e296ee05ae2584bad64b40
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2021-07-28 09:16:51 -0500

    Ventana basica y calculadora

---
+++ src/main/java/co/edu/utp/misiontic2022/c2/cdiaz/componentes/controlador/Operacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.misiontic2022.c2.cdiaz.componentes.controlador;
+
+/**
+ *
+ * @author ROG
+ */
+public enum Operacion {
+    SUMA, RESTA, MULTIPLICACION, DIVISION
+}
